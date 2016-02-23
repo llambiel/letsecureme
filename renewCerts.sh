c190cd6fad57ea21b8edaa158bc0cb43b667a3b2
Loic Lambiel <lll@exoscale.ch>
2016-02-23 16:42:20 +0100

    Renew cert script

---
+++ renewCerts.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This script renew all the Let's Encrypt certificates with a validity < 30 days
+
+if ! /opt/letsencrypt/letsencrypt-auto renew > /var/log/letsencrypt/renew.log 2>&1 ; then
+    echo Automated renewal failed:
+    cat /var/log/letsencrypt/renew.log
+    exit 1
+fi
+service nginx reload
