53829d8d8f9ab74c1faa9ef0b99a1ccaffca1f00
Loic Lambiel <lll@exoscale.ch>
2016-02-25 11:33:14 +0100

    fix typo

--- renewCerts.sh
+++ renewCerts.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# This script renew all the Let's Encrypt certificates with a validity < 30 days
+# This script renews all the Let's Encrypt certificates with a validity < 30 days
 
 if ! /opt/letsencrypt/letsencrypt-auto renew > /var/log/letsencrypt/renew.log 2>&1 ; then
     echo Automated renewal failed:
